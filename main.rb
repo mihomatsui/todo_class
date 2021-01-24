598de2724465ffe1cbfdbb519ec2960cc2a70224
mihomatsui <mihomatsui.24@gmail.com>
2021-01-24 16:20:55 +0900

    IDの自動付与

--- main.rb
+++ main.rb
@@ -1,7 +1,20 @@
 class Task
   # コードを追加
+  attr_reader :id, :title, :content
+  @@count = 0
+
+  def initialize(**params)
+    @id = @@count += 1
+    @title = params[:title]
+    @content = params[:content]
+  end
 end
 
-class Todo
-  # コードを追加
-end
+
+task1 = Task.new(title: "洗濯", content: "7時半までに干し終える")
+task2 = Task.new(title: "仕事", content: "9時〜18時")
+task3 = Task.new(title: "買い物", content: "卵,ヨーグルト")
+
+puts task1.id
+puts task2.id
+puts task3.id
