ba66968eae1974a61a0ac13b0bb2a8ed4a811c01
mihomatsui <mihomatsui.24@gmail.com>
2021-01-24 17:53:34 +0900

    infoメソッドを追加

--- main.rb
+++ main.rb
@@ -1,12 +1,15 @@
 class Task
-  # コードを追加
   attr_reader :id, :title, :content
   @@count = 0
 
-  def initialize(**params)
+  def initialize(title:, content:)
     @id = @@count += 1
-    @title = params[:title]
-    @content = params[:content]
+    @title = title
+    @content = content
+  end
+
+  def info
+    puts "[No.#{@id}] #{@title}:#{@content}"
   end
 end
 
@@ -15,6 +18,6 @@ task1 = Task.new(title: "洗濯", content: "7時半までに干し終える")
 task2 = Task.new(title: "仕事", content: "9時〜18時")
 task3 = Task.new(title: "買い物", content: "卵,ヨーグルト")
 
-puts task1.id
-puts task2.id
-puts task3.id
+puts task1.info
+puts task2.info
+puts task3.info
