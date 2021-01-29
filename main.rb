69619d5dc71963008ae56c579c29cd648d5f9efb
mihomatsui <mihomatsui.24@gmail.com>
2021-01-29 22:11:16 +0900

    add taskメソッドにtitleを追加

--- main.rb
+++ main.rb
@@ -16,20 +16,25 @@ end
 class ToDo
   def initialize
     @tasks = []
+
   end
 
   def add(task)
     # コードを追記
+    @tasks << task.title
+    puts "#{@tasks}"
   end
 
   def info
     # コードを追記
-  end
 
+  end
+end
 task1 = Task.new(title: "洗濯", content: "7時半までに干し終える")
 task2 = Task.new(title: "仕事", content: "9時〜18時")
 task3 = Task.new(title: "買い物", content: "卵,ヨーグルト")
 
-puts task1.info
-puts task2.info
-puts task3.info
+todo = ToDo.new
+todo.add(task1)
+todo.add(task2)
+# todo.info
