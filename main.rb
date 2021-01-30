ecc78c6d3e199312bceee4eaf441c1c22740e73b
mihomatsui <mihomatsui.24@gmail.com>
2021-01-31 00:06:33 +0900

    todoクラスを修正

--- main.rb
+++ main.rb
@@ -19,9 +19,16 @@ class ToDo
   end
 
   def add(task)
+    @tasks << task
+    puts "【追加】[No.#{task.id}]#{task.title}:#{task.content}"
   end
 
   def info
+    puts " *=*=*=*=*=*=*=*=* task *=*=*=*=*=*=*=*=* "
+      @tasks.each do |task|
+        puts task.info
+      end
+    puts "*=*=*=*=*=*=*=*=*=*=*=**=*=*=*=*=*=*=*=*"
   end
 end
 
@@ -29,7 +36,7 @@ task1 = Task.new(title: "洗濯", content: "7時半までに干し終える")
 task2 = Task.new(title: "仕事", content: "9時〜18時")
 task3 = Task.new(title: "買物", content: "卵, ヨーグルト")
 
-todo = Todo.new
+todo = ToDo.new
 todo.add(task1)
 todo.add(task2)
 todo.info
