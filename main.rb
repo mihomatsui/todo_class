b0ce9e89bf5770b9f6ae86637c37faf4cca91286
mihomatsui <mihomatsui.24@gmail.com>
2021-01-30 23:02:48 +0900

    task.todoメソッドを修正

--- main.rb
+++ main.rb
@@ -9,32 +9,27 @@ class Task
   end
 
   def info
-    puts "[No.#{@id}] #{@title}:#{@content}"
+   "[No.#{@id}] #{@title}:#{@content}"
   end
 end
 
 class ToDo
   def initialize
     @tasks = []
-
   end
 
   def add(task)
-    # コードを追記
-    @tasks << task.title
-    puts "#{@tasks}"
   end
 
   def info
-    # コードを追記
-
   end
 end
+
 task1 = Task.new(title: "洗濯", content: "7時半までに干し終える")
 task2 = Task.new(title: "仕事", content: "9時〜18時")
-task3 = Task.new(title: "買い物", content: "卵,ヨーグルト")
+task3 = Task.new(title: "買物", content: "卵, ヨーグルト")
 
-todo = ToDo.new
+todo = Todo.new
 todo.add(task1)
 todo.add(task2)
-# todo.info
+todo.info
